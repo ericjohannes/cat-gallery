901367e7a3c0e1d436c4ffcb014f29e7c00935b9
eric <eric.j.blom@gmail.com>
2023-03-16 14:28:09 -0500

    handling errors and updating page

---
+++ README.md
@@ -0,0 +1,11 @@
+# how this started
+`npm create vite@latest vanilla-cats-app`
+
+# to run
+`npm run dev`
+
+
+# todo
+1. get one image
+2. make it update content when request returns
+3. handle bad request and report it
--- main.js
+++ main.js
@@ -3,22 +3,46 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+// https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=REPLACE_ME
+const apiUrl =' https://api.thecatapi.com/v1/images/search'
+const apiKey = 'live_5P0mPUMV4pdr8rIzSdqnMDNtCvF5FYXq3YWdVFtzim6xTA0ig5QCGb9FaBNGVJ1X'
+
+const updatePage = ()=>{
+  return document.querySelector('#app').innerHTML = `
+  <p>Hello world!<p>
+  `
+}
+const reportError = ()=>{
+  return document.querySelector('#app').innerHTML = `
+  <p>Could not retrieve any cats!<p>
+  `
+}
+fetch(apiUrl + new URLSearchParams({
+  api_key: apiKey,
+}))
+.then(response=>{
+  if(response.status !=200){
+    return reportError()
+  }
+  return updatePage();
+})
+
+// document.querySelector('#app').innerHTML = `
+//   <div>
+//     <a href="https://vitejs.dev" target="_blank">
+//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+//     </a>
+//     <h1>Hello Vite!</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite logo to learn more
+//     </p>
+//   </div>
+// `
+
