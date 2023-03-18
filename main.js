661b3996ab96a2f8089083d2057d73c024975fc8
eric <eric.j.blom@gmail.com>
2023-03-18 10:58:06 -0500

    made back button work

--- main.js
+++ main.js
@@ -1,31 +1,61 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 import { apiUrl, apiKey } from './api.js';
-
-
 // https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=REPLACE_ME
 
-const updatePage = ()=>{
-  return document.querySelector('#app').innerHTML = `
-  <p>Hello world!<p>
-  `
+const handleBreed = (breedsArray)=>{
+  if(breedsArray.length){
+    return `Photo of a ${breedsArray[0].name}`
+  }
+  else{
+    return 'Photo of a cat.'
+  }
+}
+const updatePage = (data)=>{
+  if(window.pageCtr > 0){
+    document.querySelector('#previous').style.display = "block"; 
+
+  }
+  else{
+    document.querySelector('#previous').style.display = "none"; 
+
+  }
+  let innerHTML = '';
+  data.forEach(row=>{
+    innerHTML += `<article class="cat-info">
+    <figure class="cat-photo">
+      <img src="${row.url}" loading="lazy"  alt="${handleBreed(row.breeds)}">
+      <figcaption>${handleBreed(row.breeds)}</figcaption>
+    </figure>
+  </article>`
+  })
+  return document.querySelector('main').innerHTML = innerHTML;
+
 }
 const reportError = ()=>{
-  return document.querySelector('#app').innerHTML = `
-  <p>Could not retrieve any cats!<p>
+  return document.querySelector('#error').innerHTML = `
+  Could not retrieve more cats!
   `
 }
-fetch(apiUrl + new URLSearchParams({
-  api_key: apiKey,
-}))
-.then(response=>{
-  if(response.status !=200){
-    return reportError()
-  }
-  return updatePage();
-})
+window.pageCtr = 0;
+window.getData = (whichWay)=>{
+  const ctr = whichWay == 'more' ? 1: -1;
+  window.pageCtr += ctr;
+
+  const page = window.pageCtr;
+  fetch(apiUrl + new URLSearchParams({
+    api_key: apiKey,
+    page: page,
+    limit: 20,
+  }))
+  .then(response=>{
+    if(response.status !=200){
+      return reportError()
+    }
+    return  response.json()
+      .then(result=>{ updatePage(result)});
+  })
+
+}
+
 
 // document.querySelector('#app').innerHTML = `
 //   <div>
