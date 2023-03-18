a44075532ab36599cf6e28230cdd731050cac641
eric <eric.j.blom@gmail.com>
2023-03-18 10:08:18 -0500

    have cats loading

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import handlebars from 'vite-plugin-handlebars';
+import data from './data.json';
+
+export default {
+  plugins: [
+    handlebars({
+      context: {
+        data: data,
+      },
+    }),
+  ],
+};
