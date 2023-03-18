a4c48387964a431097f121edb794f6ce5c819bf2
eric <eric.j.blom@gmail.com>
2023-03-18 10:58:48 -0500

    clean up

--- main.js
+++ main.js
@@ -16,7 +16,6 @@ const updatePage = (data)=>{
   }
   else{
     document.querySelector('#previous').style.display = "none"; 
-
   }
   let innerHTML = '';
   data.forEach(row=>{
@@ -55,23 +54,3 @@ window.getData = (whichWay)=>{
   })
 
 }
-
-
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
-
